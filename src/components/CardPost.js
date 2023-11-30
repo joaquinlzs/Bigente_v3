22419be3f071c4d9fdee30b6d8a29ff0d0d6f124
joaquinlzs <lazojoaquin.a@gmail.com>
2023-11-30 08:43:12 -0300

    set the correct size of cards

--- src/components/CardPost.js
+++ src/components/CardPost.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import '../styles/CardPost.css';
 import getCardSize from '../utility/cardSize';
+import milisegundosPasados from '../utility/milisegundosPasados';
+import fechaTexto from '../utility/fechaTexto';
 
 function CardPost({ title, subreddit, selftext, score, permalink, created, thumbnail }) {
     const [flipped, setFlipped] = useState(false);
@@ -15,13 +17,16 @@ function CardPost({ title, subreddit, selftext, score, permalink, created, thumb
 
             <div className={`front ${flipped ? 'hidden' : ''}`}>
                 <div className='title'>
-                    {title}
+                    <a href={`https://www.reddit.com${permalink}`} target="blank">
+                        {title}
+                    </a>
                 </div>
             </div>
 
             <div className={`reverse ${flipped ? '' : 'hidden'}`}>
                 <div className='data'>
-                    Pubblicado en {subreddit} hace {created}. Tiene un puntaje de {score}.
+                    Publicado en {subreddit} hace {fechaTexto(milisegundosPasados(created))} vía Reddit. <br />
+                    Tiene un puntaje de {score}.
                 </div>
             </div>
 
