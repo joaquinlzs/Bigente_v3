be2ac76d0771e414e02b905e0780d9ae61e857dc
joaquinlzs <lazojoaquin.a@gmail.com>
2023-12-18 08:50:33 -0300

    add color to cards

--- src/components/CardPost.js
+++ src/components/CardPost.js
@@ -3,17 +3,25 @@ import '../styles/CardPost.css';
 import getCardSize from '../utility/cardSize';
 import milisegundosPasados from '../utility/milisegundosPasados';
 import fechaTexto from '../utility/fechaTexto';
+import backgroundColor from '../utility/backgroundColor';
 
 function CardPost({ title, subreddit, selftext, score, permalink, created, thumbnail }) {
     const [flipped, setFlipped] = useState(false);
 
     const flipCard = () => {
         setFlipped(!flipped);
-
+    };
+    
+    const bgColor = backgroundColor(subreddit)
+    const style = {
+        backgroundColor: bgColor,
+        color: 'white'
     };
 
+    
+
     return (
-        <div className={`postCard ${flipped ? 'flipped' : ''} ${getCardSize(score)}`} >
+        <div style={style} className={`postCard ${flipped ? 'flipped' : ''} ${getCardSize(score)}`} >
 
             <div className={`front ${flipped ? 'hidden' : ''}`}>
                 <div className='title'>
@@ -27,7 +35,7 @@ function CardPost({ title, subreddit, selftext, score, permalink, created, thumb
             <div className={`reverse ${flipped ? '' : 'hidden'}`}>
                 <div className='data'>
                     Publicado en {subreddit} hace {fechaTexto(milisegundosPasados(created))} vía Reddit. <br />
-                    Puntaje: {score}.
+                    Puntaje: {score}. Color: {backgroundColor(subreddit)}
                 </div>
             </div>
             <div className="flipButton" onClick={flipCard}>
---
+++ src/utility/backgroundColor.js
@@ -0,0 +1,20 @@
+function backgroundColor(topic) {
+    switch (topic) {
+        case 'chile':
+                return '#4d9de0';
+        case 'Subreddit ChileFit':
+                return '#ff8352';
+        case 'RepublicadeChile':
+                return '#7776bc';
+        case 'IT Chile':
+                return '#57c78b';
+        case 'Santiago':
+                return '#71d6ca';
+        case 'asklatinamerica':
+                return '#7776bc';
+        default:
+                return '#fa686a';
+    }
+}
+
+export default backgroundColor;
